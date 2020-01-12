46848a3270e261227cc47e379a1669e9744cfbfa
Alexey Uzlov <auzlov@codemastersintl.com>
2020-01-12 14:50:19 +0300

    * added ir remote sample

--- sample.ino
+++ sample.ino
@@ -1,18 +1,51 @@
-// https://playground.arduino.cc/Main/LM35HigherResolution/
-float tempC;
-int reading;
-int tempPin = 0;
+#include <IRremote.h>
+
+int RECV_PIN = 11;
+
+const int LED_PIN = 9;
+boolean ledValue = false;
+const unsigned long VALUE = 3772784863;
+unsigned long latest = -1;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
 
 void setup()
 {
-    analogReference(INTERNAL);
-    Serial.begin(9600);
+  Serial.begin(9600);
+  // In case the interrupt driver crashes on setup, give a clue
+  // to the user what's going on.
+  Serial.println("Enabling IRin");
+  irrecv.enableIRIn(); // Start the receiver
+  Serial.println("Enabled IRin");
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
-    reading = analogRead(tempPin);
-    tempC = reading / 9.31;
-    Serial.println(tempC);
-    delay(1000);
+  if (irrecv.decode(&results))
+  {
+    Serial.println(results.value);
+
+    if (results.value == VALUE) {
+      toggleLED();
+      delay(100);
+    }
+
+    irrecv.resume(); // Receive the next value
+  }
+
+  delay(100);
+}
+
+void toggleLED() {
+  ledValue = !ledValue;
+  if (ledValue) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
+  
+  Serial.println("Success, ledValue is: " + String(digitalRead(LED_PIN)));
 }
